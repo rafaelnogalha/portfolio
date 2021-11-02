99942ae5d47585ad75585c9a883490d97d73e67f
rafaelnogalha <rafaelnogalha@gmail.com>
2021-11-02 03:16:47 -0300

    fixed somethings

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,9 +9,9 @@ export default function About(){
       </Head>
       <div className={styles.contentContainer}>
         <h1>
-        I'm a technician, graduated, in <span className="curse">Systems Development</span> from the Federal Institute 
+        I'm a technician, graduated, in <span>Systems Development</span> from the Federal Institute 
         of Education, Science and Technology of Brasília. Now,
-        i'm current student of <span className="curse">Computer Science</span> at University of Brasilia,
+        i'm current student of <span>Computer Science</span> at University of Brasilia,
         i'm 21 years old and learning about clean code with <span>NodeJs</span>, <span>ReactJs</span> and
         <span> NextJs</span>. I have experience with <span>C/C++</span> languages and <span>Python</span>. 
         Everyday, i have been learning about something new. So, let's work together!
