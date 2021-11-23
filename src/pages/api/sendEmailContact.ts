96465f8a42109b06deb466342406625288d7a65e
rafaelnogalha <rafaelnogalha@gmail.com>
2021-11-23 14:16:24 -0300

    created contact form

---
+++ src/pages/api/sendEmailContact.ts
@@ -0,0 +1,22 @@
+import * as nodemailer from "nodemailer";
+
+export default function sendEmail(req, res){
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth:{
+      user: process.env.USERMAIL,
+      pass: process.env.PASSMAIL
+    }
+  })
+
+  transporter.sendMail({
+    from: req.body.email,
+    to: process.env.USERMAIL,
+    replyTo: req.body.email,
+    subject: "CONTACT | PORTFOLIO RAFAEL NOGALHA", 
+    text: req.body.message
+  }).then((response)=> { res.send(response) })
+    .catch((error) => { res.send(error) })
+}
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -6,12 +6,42 @@ import {
   Box, 
   Input, 
   Textarea, 
-  Button,
   Flex,
-  Heading
+  Heading,
+  Button
 } from '@chakra-ui/react'
 
 export default function Contact(){
+
+  function sendEmail(){
+    const formContact = document.getElementById('formContact')
+    console.log(formContact)
+
+    formContact?.addEventListener('submit', (e) =>{
+      e.preventDefault()
+      const name = (document.getElementById('name') as HTMLInputElement).value
+      const email = (document.getElementById('email') as HTMLInputElement).value
+      const message = (document.getElementById('message') as HTMLInputElement).value
+      console.log(name)
+      
+      fetch('/api/sendEmailContact', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          message
+        })
+      }).then((response) => {
+        alert("Message success sent")
+      }).catch((error) => {
+        
+      })
+    })
+  }
+
   return(
     <>
       <Head>
@@ -42,26 +72,33 @@ export default function Contact(){
                   For we work <Text as="span" color={'blue.500'}>together</Text> ✌️
                 </Text>
               </Stack>
-              <FormControl id="name">
-                <Input placeholder="Name" type="name" />
-              </FormControl>
-              <FormControl id="email">
-                <Input placeholder="Email" type="email" />
-              </FormControl>
-              <FormControl id="message">
-                <Textarea placeholder="Message" type="textarea" />
-              </FormControl>
-              <Stack spacing={10}>
-                <Button
-                  bg={'blue.500'}
-                  color={'gray.900'}
-                  _hover={{
-                    bg: 'blue.500',
-                  }}>
-                  Send
-                </Button>
+                <form 
+                  id="formContact"  
+                >
+                  <FormControl>
+                    <Input id="name" placeholder="Name" type="name" />
+                  </FormControl>
+                  <FormControl>
+                    <Input id="email" placeholder="Email" type="email" />
+                  </FormControl>
+                  <FormControl>
+                    <Textarea id="message" placeholder="Message" type="textarea" />
+                  </FormControl>
+                    
+                  <Stack spacing={10}>
+                      <Button
+                        onClick={sendEmail}
+                        type="submit"
+                        bg={'blue.500'}
+                        color={'gray.900'}
+                        _hover={{
+                          bg: 'blue.500',
+                        }}>
+                        Send
+                      </Button>
+                    </Stack>
+                </form>
               </Stack>
-            </Stack>
           </Box>
         </Stack>
       </Flex>
