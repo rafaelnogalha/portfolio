874b671cdc5cd0cdde36f5aa69aad7327db58cd3
Rafael Henrique Nogalha de Lima <rafaelnogalha@gmail.com>
2022-05-06 23:12:09 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,13 +5,13 @@ export default class MyDocument extends Document{
     return(
       <Html>
         <Head>
-          <!-- Google Tag Manager -->
-          <script dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N38SLT5');`,
+         <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N38SLT5');`,
             }}
           />
           <!-- End Google Tag Manager -->
@@ -24,9 +24,8 @@ export default class MyDocument extends Document{
           <NextScript />
           <!-- Google Tag Manager (noscript) -->
           <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N38SLT5"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N38SLT5" height="0" width="0" style="display: none; visibility: hidden;" />`,
             }}
           />
           <!-- End Google Tag Manager (noscript) -->
