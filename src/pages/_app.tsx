4aa025845d4a72d3b91380599fce6ccd26c00fc3
Rafael Henrique Nogalha de Lima <rafaelnogalha@gmail.com>
2022-05-06 22:58:42 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,25 @@
-import { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import Script from 'next/script';
+
 import { ChakraProvider } from '@chakra-ui/react';
 import { Header } from '../components/Header';
-import { theme } from '../styles/theme'
+import { theme } from '../styles/theme';
 import Footer from '../components/Footer';
-
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
     <>
       <ChakraProvider theme={theme}>
         <Header />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM-N38SLT5}');
+          `}
+        </Script>
         <Component {...pageProps} />
         <Footer />
       </ChakraProvider>
